e0e9ca493ce41641f659ee443761243226bd9055
Santiago_Rueda_Q <161849446+Santiago-Rueda-Q@users.noreply.github.com>
2025-11-04 10:22:32 -0500

    Version 3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,16 +3,16 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser },
-    rules: {
-      semi: ["error", "always"],
-      quotes: ["error", "double"],
-      "no-var": "error",
-      "no-console": "warn"
-    }
-  },
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        plugins: { js },
+        extends: ["js/recommended"],
+        languageOptions: { globals: globals.browser },
+        rules: {
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        "no-var": "error",
+        "no-console": "warn"
+        }
+    },
 ]);
